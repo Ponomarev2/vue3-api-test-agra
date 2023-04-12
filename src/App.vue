b93a8d937c84805cc47d8bc8c8523a06f689bdf6
Vasily Ponomarev <vasilyponomarev233@gmail.com>
2023-04-12 10:02:04 +0300

    cors proxy added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 
 import { ref, onMounted } from 'vue'
 import Table from './components/Table.vue';
@@ -8,16 +8,44 @@ const art = ref('OC47');
 const headers = {
     'login': '493358_stroyzar',
     'pass': 'sAVDkrEbqd',
-    'art': art
+    'art': art,
 };
+const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
 
-async function getData(headers) {
-  const response = await fetch('https://api.forum-auto.ru/v2/listGoods', { headers });
+async function getData() {
+  const response = await fetch(cors_api_url + 'https://api.forum-auto.ru/v2/listgoods/?' + 
+    'login=' + headers.login + '&' +
+    'pass=' + headers.pass + '&' +
+    'art=' + headers.art.value);
   const data = await response.json();
   console.log(data);
   artData.value = data;
 }
 
+// function doCORSRequest(options) {
+//   var x = new XMLHttpRequest();
+//   x.open(options.method, cors_api_url + options.url);
+//   x.onload = x.onerror = function() {
+//     console.log(
+//       options.method + ' ' + options.url + '\n' +
+//       x.status + ' ' + x.statusText + '\n\n' +
+//       (x.responseText || '')
+//     );
+//   };
+//   if (/^POST/i.test(options.method)) {
+//     x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//   }
+//   for(let key in options.headers){
+//     xhr.setRequestHeader(key, options.headers[key]) 
+//   }
+//   x.send(options.data);
+// }
+
+onMounted(() => {
+  // const response = doCORSRequest( { method: 'GET', url: 'https://api.forum-auto.ru/v2/listGoods', headers: headersArt } );
+  // console.log(response);
+  getData();
+})
 
 </script>
 
